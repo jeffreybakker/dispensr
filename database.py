1d413ad79f2f4380d304a14ad6c470b8de9eec05
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2016-10-31 12:32:51 +0100

    starded with the database setup

--- database.py
+++ database.py
@@ -3,13 +3,16 @@ import sqlite3 as sqlite
 conn = None
 
 
-def init(file):
+def init(file, first_time=False):
     global conn
 
     if conn is not None:
         close()
 
     conn = sqlite.connect(file)
+
+    if first_time:
+        setup()
 
 
 def close():
@@ -22,3 +25,39 @@ def close():
 def get_cursor():
     global conn
     return conn.cursor()
+
+
+def setup():
+    c = get_cursor()
+
+    c.execute("CREATE TABLE Users (id INTEGER, rfid INTEGER, role VARCHAR(3), username VARCHAR(255), password VARCHAR(255))");
+
+# CREATE TABLE Users
+# (
+#   	id			INTEGER(11)		PRIMARY KEY		NOT NULL,
+#   	rfid		INTEGER(11)		NOT NULL 		UNIQUE,
+#   	role		VARCHAR(3)		DEFAULT 'pat',
+#   	username	VARCHAR			DEFAULT NULL,
+#   	password	VARCHAR			DEFAULT NULL
+# );
+#
+# CREATE TABLE Prescription
+# (
+#   	id			INTEGER(12)		PRIMARY KEY		NOT NULL,
+#   	uid			INTEGER(11)		NOT NULL,
+#   	medicine_id	INTEGER			NOT NULL,
+#   	descr		TEXT,
+#   	max_dose	INTEGER			NOT NULL,
+#   	rec_dose	INTEGER			NOT NULL,
+#   	min-time	INTEGER			NOT NULL,
+#   	amount		INTEGER			NOT NULL
+# );
+#
+# CREATE TABLE Inventory
+# (
+# 	id 			INTEGER			PRIMARY KEY		NOT NULL,
+#   	name		VARCHAR			NOT NULL,
+#   	type		VARCHAR(3)		NOT NULL,
+#   	capacity	INTEGER			NOT NULL,
+#   	stock		INTEGER			DEFAULT 0
+# );
