a92c98fb5172d1f0ed59261a4a9b56676d79572e
Frank Stapel <frank.5voor9@gmail.com>
2016-11-03 13:49:06 +0100

    Sampledata commenting

--- sampledata.py
+++ sampledata.py
@@ -1,15 +1,12 @@
 def init():
     import calendar
-
     import time
-
     import database
     from user import User
     from prescription import Prescription
     from inventory import Inventory
 
-
-    # id, rfid, role(pat, doc, ref), username, password
+    # User class contains: id, rfid, role(pat, doc, ref), username, password
     users = [
         User(1, 2654408544, "pat", "", ""),  # Lars student card
         User(2, 3519617066, "doc", "c.s.jansen", "qwerty"),  # Martijn OV - chip card personal
@@ -23,14 +20,16 @@ def init():
         User(10, 3254740226, "pat", "", "")  # Arwin OV - chip card personal
     ]
 
-    # id, uid(id_patient), medicine_id, desc, max_dose, min_time, amount, cur_dose, last_time, doctor(id_doctor), date, duration
+    # Prescription class contains: id, uid(id_patient), medicine_id, desc, max_dose, min_time, amount, cur_dose,
+    # last_time, doctor(id_doctor), date, duration
     prescriptions = [
         Prescription(1, 3, 2, "Tegen de hoofdpijn", 3, 5, 2, 0, 0, 0, calendar.timegm(time.gmtime()), 53135135130),
         Prescription(2, 4, 3, "Niet op kauwen", 4, 10, 1, 0, 0, 0, calendar.timegm(time.gmtime()), 6843515430),
-        Prescription(3, 2, 1, "Tegen de hoofdpijn, oplossen in een bodempje water", 5, 2, 1, 0, 0, 0, calendar.timegm(time.gmtime()),45413516840),
+        Prescription(3, 2, 1, "Tegen de hoofdpijn, oplossen in een bodempje water", 5, 2, 1, 0, 0, 0,
+                     calendar.timegm(time.gmtime()), 45413516840)
     ]
 
-    # id, name, type, capacity, stock
+    # Inventory class contains: id, name, type, capacity, stock
     drugs = [
         Inventory(1, "Paracetamol", "sol", 500, 411),
         Inventory(2, "Hoestdrank", "liq", 500, 414),
@@ -47,13 +46,17 @@ def init():
 
     database.init("data/database.db", True)
 
+    # Insert all the users in users into the database
     for user in users:
         database.insert_user(user)
 
+    # Insert all prescriptions in prescriptions into the database
     for prescription in prescriptions:
         database.insert_prescription(prescription)
 
+    # Insert all drugs in drugs into the database
     for drug in drugs:
         database.insert_inventory(drug)
 
+    # Commits all database changes mad since the last commit
     database.commit()
