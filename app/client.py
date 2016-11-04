871e5b925c44fd87ad3835170cda91e963de80bd
Kevin Witlox <kevin.witlox@upcmail.nl>
2016-11-04 18:42:50 +0100

    f u

--- app/client.py
+++ app/client.py
@@ -9,7 +9,6 @@ import time
 from prescription import Prescription
 from tkinter import messagebox
 from ui_login import UILogin
-from ui_loading import UILoading
 from ui_main import UIMain
 
 HOST, PORT = "127.0.0.1", 9999
@@ -19,29 +18,30 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 prescriptions = None
 uid = -1
 
+
+# This function is used so that the UILogin can pass the UID to the main thread
 def app_set_uid(value):
     global uid
     uid = value
     print('uid set')
 
+
+# Set running to False so that the loops of all threads stop
+# Send the shutdown signal so that socket.recv stops blocking the threads (otherwise they wont exit)
 def app_close():
     running = False
     s.shutdown(1)
     print("running = False")
 
 
-def app_main(sock, func_close, func_queue):
-    print("executing app_main")
-    ui_main = UIMain(sock, func_close, func_queue)
-
-
+# Stop the Queue from blocking the main thread (see bottom for further explanation)
 def unblock_main():
     global blocking
     blocking = False
     print("Unblocking Main Thread")
 
 
-# Connection thread
+# Thread used to continuously receive data
 class connection_thread(threading.Thread):
     def __init__(self, threadID, name, sock):
         threading.Thread.__init__(self)
@@ -65,11 +65,13 @@ class connection_thread(threading.Thread):
                 json_data = received.decode()
 
             if json_data != "":
+                # Messages are send in JSON format
                 data = json.loads(json_data)
                 print("Received: " + str(json_data))
 
                 if data["command"] == "authlogin":
                     if data["auth"] == "True":
+                        # When the server accepts the login request, send a new request to the server in order to retrieve the prescriptions of the patient
                         tosend = {}
                         tosend["command"] = "getprescriptions"
                         print("uid: " + str(uid))
@@ -77,17 +79,17 @@ class connection_thread(threading.Thread):
                         print("Sending: " + str(json.dumps(tosend)))
                         self.sock.send(json.dumps(tosend).encode())
                     elif data["auth"] == "False":
-                        #messagebox.showerror("Error", "Authentication failed")
+                        # The authentication failed (because of wrong credentials), exit the program
+                        # messagebox.showerror("Error", "Authentication failed")
                         print("Authentication failed")
                         running = False
                         s.shutdown(1)
                 if data["command"] == "getprescriptions":
+                    # The request to retrieve the prescriptions has been answered, convert them to prescription objects and notify the main thread
                     global prescriptions
                     prescriptions = {}
                     prescriptions = Prescription.from_json_list(data["data"])
-                    from_dummy_thread(lambda : {unblock_main()})
-
-
+                    from_dummy_thread(lambda: {unblock_main()})
 
         print("Exiting " + self.name)
         from_dummy_thread(lambda: unblock_main())
@@ -116,10 +118,6 @@ def from_main_thread_nonblocking():
         callback()
 
 
-def update_queue():
-    from_main_thread_blocking()
-
-
 # Main Code
 try:
     s.connect((HOST, PORT))
@@ -128,21 +126,38 @@ except socket.error as msg:
     messagebox.showerror("Error", 'Connection failed.')
     sys.exit()
 
+# Start the thread that listens to the socket
 connthread = connection_thread(1, "Connection Thread", s)
 connthread.start()
-ui_login = UILogin(s, app_close, app_set_uid)
+# Start the login ui to get the credentials of the user, this will then send the authentication request and close itself so that the main thread can continue
+# So important to note is that the UILogin runs on the main thread, which means that the code beneath it is only ran
+# once the UILogin is closed (by entering the credentials and pressing login, or pressing the x button)
+# Note: app_close is passed on so that UILogin can set running to false in order to prevent the program from continuing
+# Note app_set_uid is passed on so that UILogin can pass on the uid of which the prescriptions have to be read
+UILogin(s, app_close, app_set_uid)
 
 blocking = True
 print("Blocking Main Thread")
 while blocking:
+    # This will stop the main thread from going past this while loop.
+    # from_main_thread_blocking will keep checking the Queue
+    # once the connection thread receives an answer to the authentication request, it will add the unblock_main() method
+    # to the Queue, which will set blocking to false and stop from_main_thread_blocking() from blocking the main thread
     from_main_thread_blocking()
 print("Stopped blocking Main Thread")
 
+# If no exit signal has been given yet, e.g. if the wrong credentials were entered, continue the program by opening
+# the main ui
+# Note: prescriptions is passed on so that the Main UI has data to display
 if running:
-    UIMain(s, app_close, prescriptions)
+    UIMain(s, prescriptions)
+    app_close()
 
-
+# Wait for the thread to close
+# Note: if the connection thread is not properly notified that the program want to exit, e.g. no shutdown signal is given,
+# it will prevent the program from exiting
 connthread.join()
+# just to be sure
 s.shutdown(1)
+# close the socket
 s.close()
-
--- app/ui_main.py
+++ app/ui_main.py
@@ -7,51 +7,65 @@ from tkinter.ttk import Treeview
 
 
 class UIMain:
-    def __init__(self, sock, func_close, prescriptions):
-        root = tkinter.Tk()
+    if __name__ == '__main__':
+        if __name__ == '__main__':
+            if __name__ == '__main__':
+                def __init__(self, sock, prescriptions):
+                    # Create the window
+                    root = tkinter.Tk()
 
-        tree = Treeview(root, columns=(
-            'uid', 'medicine_id', 'descr', 'max_dose', 'min_time', 'amount', 'cur_dose', 'last_time',
-            'doctor',
-            'date', 'duration'))
-        tree.heading('#0', text='id')
-        tree.heading('uid', text='uid')
-        tree.heading('medicine_id', text='medicine_id')
-        tree.heading('descr', text='descr')
-        tree.heading('max_dose', text='max_dose')
-        tree.heading('min_time', text='min_time')
-        tree.heading('amount', text='amount')
-        tree.heading('cur_dose', text='cur_dose')
-        tree.heading('last_time', text='last_time')
-        tree.heading('doctor', text='doctor')
-        tree.heading('date', text='date')
-        tree.heading('duration', text='duration')
+                    # Create the table
+                    tree = Treeview(root, columns=(
+                        'uid', 'medicine_id', 'descr', 'max_dose', 'min_time', 'amount', 'cur_dose', 'last_time',
+                        'doctor',
+                        'date', 'duration'))
+                    tree.heading('#0', text='id')
+                    tree.heading('uid', text='uid')
+                    tree.heading('medicine_id', text='medicine_id')
+                    tree.heading('descr', text='descr')
+                    tree.heading('max_dose', text='max_dose')
+                    tree.heading('min_time', text='min_time')
+                    tree.heading('amount', text='amount')
+                    tree.heading('cur_dose', text='cur_dose')
+                    tree.heading('last_time', text='last_time')
+                    tree.heading('doctor', text='doctor')
+                    tree.heading('date', text='date')
+                    tree.heading('duration', text='duration')
 
-        tree.column('#0', width=40)
-        tree.column('uid', width=40)
-        tree.column('medicine_id', width=70)
-        tree.column('descr', width=70)
-        tree.column('max_dose', width=70)
-        tree.column('min_time', width=70)
-        tree.column('amount', width=70)
-        tree.column('cur_dose', width=70)
-        tree.column('last_time', width=70)
-        tree.column('doctor', width=70)
-        tree.column('date', width=70)
-        tree.column('duration', width=70)
+                    tree.column('#0', width=40)
+                    tree.column('uid', width=40)
+                    tree.column('medicine_id', width=70)
+                    tree.column('descr', width=70)
+                    tree.column('max_dose', width=70)
+                    tree.column('min_time', width=70)
+                    tree.column('amount', width=70)
+                    tree.column('cur_dose', width=70)
+                    tree.column('last_time', width=70)
+                    tree.column('doctor', width=70)
+                    tree.column('date', width=70)
+                    tree.column('duration', width=70)
 
-        for i in range(0, len(prescriptions)):
-            tree.insert('', 'end', text=prescriptions[i].id,
-                        values=[prescriptions[i].uid, prescriptions[i].medicine_id, prescriptions[i].descr,
-                                prescriptions[i].max_dose, prescriptions[i].min_time, prescriptions[i].amount,
-                                prescriptions[i].cur_dose, prescriptions[i].last_time, prescriptions[i].doctor,
-                                prescriptions[i].date, prescriptions[i].duration])
+                    # Insert the data
+                    for i in range(0, len(prescriptions)):
+                        tree.insert('', 'end', text=prescriptions[i].id,
+                                    values=[prescriptions[i].uid, prescriptions[i].medicine_id, prescriptions[i].descr,
+                                            prescriptions[i].max_dose, prescriptions[i].min_time, prescriptions[i].amount,
+                                            prescriptions[i].cur_dose, prescriptions[i].last_time, prescriptions[i].doctor,
+                                            prescriptions[i].date, prescriptions[i].duration])
 
-        tree.pack()
+                    tree.pack()
 
-        root.mainloop()
-        func_close()
+                    # Run the mainloop
+                    root.mainloop()
 
-    def update_tree(self, prescriptions):
-        print("im update tree")
-        pass
+                    # ^^ Here i ran in to a problem since this means the 'main thread' (as in client.py) can't notify this class
+                    # of certain events because this ui technically runs on the main thread.
+                    # Therefore, in order to add functionality such as changing prescriptions, which would need to combine
+                    # this thread for receiving the ui events, the connection thread for handling the responses from the server
+                    # and the main thread for communication between the two threads, is not possible in this version of the code
+                    # Unfortunately there was not enough time to finish this UI
+                    # (also running this class on a seperate thread severely increases the difficulty of letting all
+                    # threads communicate with each other)
+
+
+
